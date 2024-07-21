cf42eee1ed90d6f1a8bef001e63402b25279f978
PuntawatSub <57529099+puntawatsub@users.noreply.github.com>
2024-07-21 23:45:40 +0700

    add new page and improvements
    add forget password and relying on firebase auth for login state instead of localStorage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ["@prisma/nuxt", "@nuxtjs/tailwindcss"],
   alias: {
-    ".prisma/client/default": "./node_modules/.pnpm/@prisma+client@5.16.1_prisma@5.16.1/node_modules/.prisma/client/default.js",
-    ".prisma/client/index-browser": "./node_modules/.pnpm/@prisma+client@5.16.1_prisma@5.16.1/node_modules/.prisma/client/index-browser.js"
+    ".prisma/client/default": "./node_modules/.pnpm/@prisma+client@5.17.0_prisma@5.17.0/node_modules/.prisma/client/default.js",
+    ".prisma/client/index-browser": "./node_modules/.pnpm/@prisma+client@5.17.0_prisma@5.17.0/node_modules/.prisma/client/index-browser.js"
   },
   imports: {
     dirs: [
---
+++ pages/fog.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <!-- <img class="mx-auto h-10 w-auto" src="path/to/logo" alt="Your Company" /> -->
+       <h1 class="text text-xl text-center font-bold">URL Shortener</h1>
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Reset password</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div class="space-y-6">
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+          <div class="mt-2">
+            <input v-model="email" id="email" name="email" type="email" autocomplete="email" required class="block px-[10px] w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+          <button @click="onReset" type="submit" class="flex w-full justify-center rounded-md bg-sky-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">Reset Password</button>
+        </div>
+      </div>
+
+      <p class="mt-10 text-center text-sm text-gray-500">
+        No account?
+        {{ ' ' }}
+        <a href="/signup" class="font-semibold leading-6 text-sky-600 hover:text-sky-500">Create new!</a>
+      </p>
+    </div>
+  </div>
+  <!-- <footer class="absolute bottom-2 w-full text-center">
+    <a class="">Copyright © 2024 <a class="font-semibold">Puntawat Subhamani.</a> All rights reserved.</a>
+  </footer> -->
+</template>
+
+<script setup lang="ts">
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+
+const app = useFirebaseApp();
+const email = ref("")
+const auth = getAuth();
+
+const onReset = () => {
+    sendPasswordResetEmail(auth, email.value)
+        .then(async () => {
+            alert("Password reset email sent, please also check your spam folder");
+            await navigateTo("/login")
+        })
+        .catch((error: any) => {
+            console.log(error.code);
+        })
+}
+</script>
